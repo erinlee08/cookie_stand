dcde3e67c829bf348aa12f436d497e7c08ecf3ae
Erin <erinylee@yahoo.com>
2016-08-22 23:42:50 -0700

    code

---
+++ seattle_center.js
@@ -0,0 +1,21 @@
+var shop3 = {
+  name: 'Seattle Center',
+  min_cust:	20,
+  max_cust:	38,
+  avg_cookie:2.3
+  custPerHour: function() {
+  min = Math.ceil;
+  max = Math.floor;
+  return Math.floor(Math.random() * (max - min + 1) + min);
+  cookiePerHour: function(){
+    return this.avg_cookie * Math.random;
+  }
+  }
+}
+var cookieHour = [cookiePerHour,custPerHour];
+// create
+var tag1 = document.getElementsByTagName(<ul>);
+                          // add content
+tag1.innnerText = <ul> + cookieHour[] + </ul>;
+// add to html
+document.body.append(<ul>);
